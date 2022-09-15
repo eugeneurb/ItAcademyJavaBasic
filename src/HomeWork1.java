cd5d9bda292c69dba61413ad788b35578d7391d7
eugeneurb <evgurbanovich@gmail.com>
2022-09-15 23:44:27 +0300

    HomeWork1

---
+++ src/HomeWork1.java
@@ -0,0 +1,20 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        short s = 0x55ff;
+        int i = 1000000;
+        long l = 0xffffffffL;
+        char c = 'a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+        System.out.println("The value of the variable b=" + b);
+        System.out.println("The value of the variable s=" + s);
+        System.out.println("The value of the variable i=" + i);
+        System.out.println("The value of the variable l=" + l);
+        System.out.println("The value of the variable c=" + c);
+        System.out.println("The value of the variable f=" + f);
+        System.out.println("The value of the variable d=" + d);
+        System.out.println("The value of the variable bool=" + bool);
+    }
+}
